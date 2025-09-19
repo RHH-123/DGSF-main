14d0f201de89af8256fda966407ac0c0853a646e
Camellia123 <2416915166@qq.com>
2025-09-19 13:42:48 +0800

    Update transform.py

--- transform.py
+++ transform.py
@@ -59,34 +59,29 @@ def add_uniform_noise(x):
     return torch.clip(x + torch.zeros_like(x).uniform_(-16/255, 16/255), 0, 1)
 
 def add_gass_noise(x):
-    gauss =  torch.randn(x.size()) * (16.0 / 255)  # 生成标准差为sigma的随机高斯噪声
+    gauss =  torch.randn(x.size()) * (16.0 / 255) 
     gauss = gauss.cuda()
     return torch.clip(x + gauss, 0, 1)
 
 def add_salt_noise(x,prob=0.01):
-    noise = torch.rand_like(x)  # 生成 [0, 1] 的均匀分布随机数
-    salt = (noise < prob / 2).float()  # 盐噪声（设置为1）
-    pepper = (noise > 1 - prob / 2).float()  # 椒噪声（设置为0）
-    noisy_x = x * (1 - salt - pepper) + salt  # 应用椒盐噪声
+    noise = torch.rand_like(x)  
+    salt = (noise < prob / 2).float()  
+    pepper = (noise > 1 - prob / 2).float()  
+    noisy_x = x * (1 - salt - pepper) + salt  
     return noisy_x
 
 def add_poisson_noise(x, scale=30):
-    """
-    向输入张量添加泊松噪声
-    :param x: 输入张量，范围应为 [0, 1]
-    :param scale: 缩放因子，用于调整噪声强度
-    :return: 添加噪声后的张量
-    """
-    device = x.device  # 确保在同一设备上
-    x_scaled = x * scale  # 将图像值放大，模拟光子计数
-    poisson_noise = torch.poisson(x_scaled).to(device)  # 生成泊松噪声
-    noisy_x = poisson_noise / scale  # 缩放回 [0, 1] 范围
+   
+    device = x.device  
+    x_scaled = x * scale  
+    poisson_noise = torch.poisson(x_scaled).to(device)  
+    noisy_x = poisson_noise / scale  
     return torch.clip(noisy_x, 0, 1)
 
 
 #对x进行随机丢弃部分像素
 def drop_out(x):
-    return F.dropout2d(x, p=0.1, training=True) #对x的通道进行二维随机失活，失活的概率为10%，相当于随机将部分像素设为0
+    return F.dropout2d(x, p=0.1, training=True) 
 
 Blur = T.GaussianBlur(kernel_size=5, sigma=(0.1, 2.0))
 solarizer = T.RandomSolarize(threshold=0.6)
